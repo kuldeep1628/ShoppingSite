624e5b196f8a2e04b5cc6a90f9b0167ec61007e6
kuldeep1628 <kuldeepchauhan1628@gmail.com>
2024-09-17 19:40:34 +0530

    Tailwind added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
-        <Signup/>
+        <NavBar/>
   );
 }
 
